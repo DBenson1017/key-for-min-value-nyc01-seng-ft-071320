c93d877b3c4c16417d5d07d543106392c1d6ab57
Devin Benson <devinbenson@gmail.com>
2020-07-02 23:08:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   lowest_number = nil
 
 name_hash.each do |name, value|
-  if value < min_value 
+  if lowest_number = nil 
+    return 
     min_value = value 
     #{name} = answer 
   end
